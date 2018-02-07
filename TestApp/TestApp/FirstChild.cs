26ac5fff290059b3516785f363da9980cb6b2fb9
Asif Chowdhury <asifrc@gmail.com>
2018-02-07 11:21:25 -0500

    Adding in TestApp

---
+++ TestApp/TestApp/FirstChild.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class FirstChild : Parent
+    {
+        public FirstChild() : base()
+        {
+             
+        }
+        public override string Work()
+        {
+            return "Slacking";
+        }
+    }
+}
---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dummy app, doesn't do anything.");
+            Parent f = new FirstChild();
+            Console.WriteLine(f.Work());
+        }
+    }
+}
